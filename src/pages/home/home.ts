f1131b5b721c5bcccb83e8bf10f9357f9e7736d9
Gundavarapu <gundavs@UTCCGL.com>
2018-12-04 09:30:05 +0530

    first commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Component, ViewChild, Renderer } from '@angular/core';
+import { NavController, Platform, Content  } from 'ionic-angular';
+import { Base64ToGallery, Base64ToGalleryOptions } from '@ionic-native/base64-to-gallery';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,118 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
-
+  // Canvas stuff
+  @ViewChild('imageCanvas') canvas: any;
+  canvasElement: any;
+ 
+  saveX: number;
+  saveY: number;
+ 
+  storedImages = [];
+ 
+  // Make Canvas sticky at the top stuff
+  @ViewChild(Content) content: Content;
+  @ViewChild('fixedContainer') fixedContainer: any;
+ 
+  // Color Stuff
+  selectedColor = '#9e2956';
+ 
+  colors = [ '#9e2956', '#c2281d', '#de722f', '#edbf4c', '#5db37e', '#459cde', '#4250ad', '#802fa3' ];
+ 
+  constructor(public navCtrl: NavController, private base64ToGallery: Base64ToGallery,
+     public renderer: Renderer, private plt: Platform) {}
+ 
+  ionViewDidEnter() {
+    // https://github.com/ionic-team/ionic/issues/9071#issuecomment-362920591
+    // Get the height of the fixed item
+    let itemHeight = this.fixedContainer.nativeElement.offsetHeight;
+    let scroll = this.content.getScrollElement();
+ 
+    // Add preexisting scroll margin to fixed container size
+    itemHeight = Number.parseFloat(scroll.style.marginTop.replace("px", "")) + itemHeight;
+    scroll.style.marginTop = itemHeight + 'px';
+  }
+ 
+  ionViewDidLoad() {
+    // Set the Canvas Element and its size
+    this.canvasElement = this.canvas.nativeElement;
+    this.canvasElement.width = this.plt.width() + '';
+    this.canvasElement.height = 200;
   }
 
+  selectColor(color) {
+    this.selectedColor = color;
+  }
+   
+  startDrawing(ev) {
+    var canvasPosition = this.canvasElement.getBoundingClientRect();
+   
+    this.saveX = ev.touches[0].pageX - canvasPosition.x;
+    this.saveY = ev.touches[0].pageY - canvasPosition.y;
+  }
+   
+  moved(ev) {
+    var canvasPosition = this.canvasElement.getBoundingClientRect();
+   
+    let ctx = this.canvasElement.getContext('2d');
+    let currentX = ev.touches[0].pageX - canvasPosition.x;
+    let currentY = ev.touches[0].pageY - canvasPosition.y;
+   
+    ctx.lineJoin = 'round';
+    ctx.strokeStyle = this.selectedColor;
+    ctx.lineWidth = 5;
+   
+    ctx.beginPath();
+    ctx.moveTo(this.saveX, this.saveY);
+    ctx.lineTo(currentX, currentY);
+    ctx.closePath();
+   
+    ctx.stroke();
+   
+    this.saveX = currentX;
+    this.saveY = currentY;
+  }
+   
+   
+  saveCanvasImage() {
+    var dataUrl = this.canvasElement.toDataURL("image/jpeg");
+   
+    let ctx = this.canvasElement.getContext('2d');
+    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height); // Clears the canvas
+   
+    var data = dataUrl.split(',')[1];
+    var options:Base64ToGalleryOptions={ prefix: '_img', mediaScanner: false };
+
+    this.base64ToGallery.base64ToGallery(data, options).then(
+      res => {
+        console.log('Saved image to gallery ', res)
+      },
+      err => {
+        console.log('Error saving image to gallery ', err)
+      });
+  }
+   
+  // https://forum.ionicframework.com/t/save-base64-encoded-image-to-specific-filepath/96180/3
+  b64toBlob(b64Data, contentType) {
+    contentType = contentType || '';
+    var sliceSize = 512;
+    var byteCharacters = atob(b64Data);
+    var byteArrays = [];
+   
+    for (var offset = 0; offset < byteCharacters.length; offset += sliceSize) {
+      var slice = byteCharacters.slice(offset, offset + sliceSize);
+   
+      var byteNumbers = new Array(slice.length);
+      for (var i = 0; i < slice.length; i++) {
+        byteNumbers[i] = slice.charCodeAt(i);
+      }
+   
+      var byteArray = new Uint8Array(byteNumbers);
+   
+      byteArrays.push(byteArray);
+    }
+   
+    var blob = new Blob(byteArrays, { type: contentType });
+    return blob;
+  }
 }
